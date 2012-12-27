f9e695d52e6a9abaa35d8554c4cbb6e606c651d9
shallker.wang <shallker.wang@profero.com>
2012-12-27 17:03:41 +0800

    Add a textarea on the left of the page when key pressed

---
+++ background.js
@@ -0,0 +1,32 @@
+chrome.tabs.onCreated.addListener(function(tab) {
+    console.log(tab);
+})
+
+chrome.commands.onCommand.addListener(function(command) {
+    console.log('command:', command);
+    if (command == 'toggle-pinned') {
+        chrome.tabs.getSelected(null, function(tab) {
+            // Toggle the pinned status
+            chrome.tabs.update(tab.id, {'pinned': !tab.pinned}); 
+       });
+    }
+})
+
+chrome.commands.getAll(function(a) {
+    console.log('getAll:', a);
+})
+
+chrome.browserAction.onClicked.addListener(function(click) {
+    console.log('click:', click);
+})
+
+// chrome.browserAction.onClicked.addListener
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log('message:', request, sender, sendResponse);
+    sendResponse(localStorage);
+    // if (request.greeting == "hello") sendResponse({farewell: "goodbye"});
+});
+
+localStorage.hello = 'earth';
+console.log('localStorage:', localStorage);
