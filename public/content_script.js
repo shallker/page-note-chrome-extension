fa34ae22210782b950b309eba0d860b8d5d5265d
shallker.wang <shallker.wang@profero.com>
2013-01-04 17:27:48 +0800

    Add original stuffs to public folder.

---
+++ public/content_script.js
@@ -0,0 +1,14 @@
+var pageUrl = window.location.origin + window.location.pathname;
+if (pageUrl == 'http://www.baidu.com/search/error.html') {
+    alert('has page note');
+}
+
+Mousetrap.bind('ctrl+`', openPageNote);
+
+function openPageNote() {
+  $('#page-note').toggle();
+}
+
+$('body').append('<div id="page-note" class="none"><textarea>hello</textarea></div>');
+$(function() {
+})
