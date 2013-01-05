9ea4853d761469638f7693d470a798ab8c4a3ff9
shallker.wang <shallker.wang@profero.com>
2013-01-06 00:33:18 +0800

    Add page note transform effect of two versions scale and translate.

--- public/content-scripts/js/page-note.js
+++ public/content-scripts/js/page-note.js
@@ -1,14 +1,41 @@
-var pageUrl = window.location.origin + window.location.pathname;
-if (pageUrl == 'http://www.baidu.com/search/error.html') {
-    alert('has page note');
+
+function addClass( element, name ) {
+    element.className = element.className.replace( /\s+$/gi, '' ) + ' ' + name;
 }
 
-Mousetrap.bind('`', openPageNote);
+function removeClass( element, name ) {
+    element.className = element.className.replace( name, '' );
+}
+
+Mousetrap.bind('`', togglePageNote);
+
+var opend = false;
+function togglePageNote() {
+    if (opend) return closePageNote();
+    return openPageNote();
+}
 
 function openPageNote() {
-  $('#page-note').toggle();
+    opend = true;
+    $(document.documentElement).addClass('page-note-init');
+    $(document.documentElement).addClass('page-note-open');
 }
 
-$('body').append('<div id="page-note" class="none"><textarea>hello</textarea></div>');
-$(function() {
-})
+function closePageNote() {
+    opend = false;
+    $(document.documentElement).removeClass('page-note-open');
+    recoverDocument();
+}
+
+function recoverDocument() {
+    window.setTimeout(function() {
+        if (!opend) {
+            $(document.documentElement).removeClass('page-note-init');
+        }
+    }, 1000)    
+}
+
+insertNote()
+function insertNote() {
+    $(document.body).append('<div id="page-note" class="page-note"><input type="text" class="note-title" ><textarea class="note-content">Hello world</textarea></div>')
+}
